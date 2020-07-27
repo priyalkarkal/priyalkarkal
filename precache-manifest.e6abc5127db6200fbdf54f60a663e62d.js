6b07fcc3076422c6ae5f9c90beff4ad61eb37cea
ved.sachdeva <ved.sachdeva@curefit.com>
2020-07-27 21:59:07 +0530

    Updates

---
+++ precache-manifest.e6abc5127db6200fbdf54f60a663e62d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0046368d0ac77ffcfc5f3c72f48f6762",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "34b90cad6238daf9ce13",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7953af0c7c7181ae8c3b",
+    "url": "/portfolio/static/js/2.6336b018.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.6336b018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b90cad6238daf9ce13",
+    "url": "/portfolio/static/js/main.226f4d58.chunk.js"
+  },
+  {
+    "revision": "9f28ddc3708e11629e63",
+    "url": "/portfolio/static/js/runtime-main.cc0ad463.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a398c487adc3f350cf286bdbd6df2c6d.js"
+  "/portfolio/precache-manifest.e6abc5127db6200fbdf54f60a663e62d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
